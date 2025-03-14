eb4de7162966f3c882b256adedf286bdedbcf47f
Santob13 <santob13@gmail.com>
2025-03-14 14:46:47 -0700

    ALB

--- main.tf
+++ main.tf
@@ -38,11 +38,11 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-
-  image_id            = data.aws_ami.app_ami.id
-  instance_type       = var.instance_type
+  
+  security_groups        = [module.blog_sg.security_group_id]
+  autoscaling_group_name = aws_autoscaling_group.blog.id
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 }
 
 module "blog_alb" {
